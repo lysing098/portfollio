e44c45a9c00d1197c515ec6156948e79e1b0fb13
git lysing098 <lyzing1021@gmail.com>
2025-12-07 20:33:59 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/pixel-perfect-portfolio/",
+  // base: "/pixel-perfect-portfolio/",
   server: {
     host: "::",
     port: 8080,
